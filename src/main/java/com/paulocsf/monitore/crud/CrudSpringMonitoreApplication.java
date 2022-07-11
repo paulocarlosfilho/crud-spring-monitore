e132365aa5da81fc1c3e2512141ddce597a1e9d1
Paulo Carlos <83230882+paulocarlosfilho@users.noreply.github.com>
2022-07-11 15:48:10 -0300

    Testando um novo repositorio para o Monitore

---
+++ src/main/java/com/paulocsf/monitore/crud/CrudSpringMonitoreApplication.java
@@ -0,0 +1,13 @@
+package com.paulocsf.monitore.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSpringMonitoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSpringMonitoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paulocsf/monitore/crud/CrudSpringMonitoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paulocsf.monitore.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSpringMonitoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
